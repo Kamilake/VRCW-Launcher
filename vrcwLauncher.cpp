3136907f220c689bd20cf25ae25e11852c0a71a8
Kamilake <exjang0@gmail.com>
2022-07-31 11:49:57 +0900

    .

--- vrcwLauncher.cpp
+++ vrcwLauncher.cpp
@@ -1,6 +1,29 @@
-#include <stdio.h>
-int main()
+#include <bits/stdc++.h>
+//main with arguments
+int main(int argc, char *argv[])
 {
-  printf("Hello World!\n");
-  return 1234;
+  //check if there are enough arguments
+  if (argc < 2)
+  {
+    //print error message
+    printf("Error: Not enough arguments\n");
+    //exit program
+    return 1;
+  }
+  //check if there are too many arguments
+  if (argc > 2)
+  {
+    //print error message
+    printf("Error: Too many arguments\n");
+    //exit program
+    return 1;
+  }
+  //check if the first argument is a valid file
+  if (fopen(argv[1], "r") == NULL)
+  {
+    //print error message
+    printf("Error: File not found\n");
+    //exit program
+    return 1;
+  }
 }
