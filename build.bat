2fc5464d7da38d3e4bae885356b7baf4164dc518
Kamilake <exjang0@gmail.com>
2023-02-02 13:28:24 +0900

    Awesome Icon!

--- build.bat
+++ build.bat
@@ -1,3 +1,5 @@
 @echo off
-gcc -o %2 %1
-%2
+@REM tasklist | find /i "%2" && taskkill /im notepad.exe /F || rem
+@REM taskkill -im %2 -f
+windres vrcw.rc -O coff -o vrcw.res
+gcc -O2 -o %2 %1 vrcw.res   
